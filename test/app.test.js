f9661360c605cd6ecaa77982ea044dd35c90b26d
Moiz-ur-Rehman-03 <moiz.urrehman@tintash.com>
2022-08-26 07:21:38 +0500

    Unit Testing
    WHAT: Unit Testing of Social Network App
    
    WHY: no changes
    
    IMPACT: HIGH

---
+++ test/app.test.js
@@ -0,0 +1,6 @@
+require('./auth'); // testing login and register of User and Moderator
+require('./user'); // testing User Routes
+require('./moderator'); // testing Moderator Routes
+require('./payment'); // testing Payment Route
+require('./post'); // testing Post Routes
+require('./feed'); // testing Feed Routes
